65595ca4ecad83b5db63f9864a02bf104e278309
wabo225 <wabo225@uky.edu>
2019-10-01 19:32:15 -0400

    activate solenoid loop without pausing main arduino loop

--- PI_Buttons/src/main.cpp
+++ PI_Buttons/src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void activateSolenoid(int Pin) {
+  // send a HIGH Signal to Pin
+  digitalWrite(Pin, HIGH);
 }
 
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i<10; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+bool act = true;
+int pi[10] = {3,1,4,1,5, 9, 2, 6, 5, 4};
+int i = 0;
+unsigned long beginDelay;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your
+  // main code here, to run repeatedly:
+
+  if(act && i < 10) {
+    activateSolenoid(i);
+    beginDelay = millis();
+    act = false;
+  } else {
+    if (millis() - beginDelay < 500) {
+      act = true;
+      i++;
+    }
+  }
 }
