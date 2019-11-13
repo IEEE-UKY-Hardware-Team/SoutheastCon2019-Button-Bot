61c3eba038520395149a3be630b04fbb7a5de78b
wabo225 <wabo225@uky.edu>
2019-11-13 13:29:01 -0500

    PI controller for one motor and changed encoder reading to work in both directions.

--- DrivingBot/src/main.cpp
+++ DrivingBot/src/main.cpp
@@ -5,13 +5,14 @@ int digital_pin_A = 8;
 int digital_pin_B = 7;
 int digital_pin_C = 6;
 int digital_pin_D = 5;
-int count = 0, last = 0, current = 0;
+int count = 0, lastA = 0, currentA = 0;
 int wheel_rotation_count = 0;
 unsigned long lastSampleClock = 0;
 unsigned long currClock = 0;
 int drivePins[2] = {digital_pin_C, digital_pin_D};
 
-PID pid1(0.4, 0, 0);
+PID pid1(0.5, 0.001, 0);
+
 
 #define motor_slowest 45
 #define motor_fastest 255
@@ -19,37 +20,41 @@ PID pid1(0.4, 0, 0);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pid1.SetFrequency(1000);
   pinMode(digital_pin_A,INPUT);
   pinMode(digital_pin_B,INPUT);
   pinMode(digital_pin_C,OUTPUT);
   pinMode(digital_pin_D,OUTPUT);
-
-  analogWrite(digital_pin_C, 0);
-  analogWrite(digital_pin_D, motor_slowest);
   lastSampleClock = 0;
+
+  // drive(drivePins, motor_slowest);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   currClock = micros();
-  current = digitalRead(digital_pin_A);
-  if (last != current && last == 0)
+  currentA = digitalRead(digital_pin_A);
+  if (lastA != currentA && lastA == 0)
   {
-    count++;
+    if (digitalRead(digital_pin_B)) {
+      count++;
+    } else {
+      count--;
+    }
+    Serial.println(count);
   }
-  //Serial.println(digitalRead(digital_pin_A));
-  last = current;
-
+  // Serial.println(digitalRead(digital_pin_A));
+  // Serial.println(count);
+  lastA = currentA;
 
   //Serial.print("Clock");  
   //Serial.println(currClock - lastSampleClock);
 
   if (currClock - lastSampleClock > 1000) {
-    int error = -(360 - count);
+    int error = 360 - count;
     drive(drivePins, pid1.evaluate(error));
-    //Serial.print("error");
-    //Serial.println(error);
-    count = 0;
+    // Serial.print("error");
+    // Serial.println(error);
     lastSampleClock = currClock;
   }
 }
