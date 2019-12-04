c5d3b56b454ba6f7e3b5eabb908bd7c697faec40
Jarren Tay <jarrentayusa@gmail.com>
2019-12-04 18:45:08 -0500

    Programmed the robot's procedure. Remove decimal point from pi

--- PI_Buttons/src/main.cpp
+++ PI_Buttons/src/main.cpp
@@ -2,18 +2,21 @@
 #include "../include/pi10000.h"
 // #include "Arduino_LCD.h"
 
-#define digit_00        2
-#define digit_01        3
-#define digit_02        4
-#define digit_03        5
-#define digit_04        6
-#define digit_05        7
-#define digit_06        8
-#define digit_07        9
-#define digit_08        10
-#define digit_09        11
-#define Error_pin       12
-#define button_pin      13
+#define digit_00          0
+#define digit_01          1
+#define digit_02          2
+#define digit_03          3
+#define digit_04          4
+#define digit_05          5
+#define digit_06          6
+#define digit_07          7
+#define digit_08          8
+#define digit_09          9
+#define drive_pin_a       10
+#define drive_pin_b       11
+#define trigger_pin       12
+#define release_arms_pin  13
+#define drive_forward     255
 
 void activateSolenoid(int Pin) {
   // send a HIGH Signal to Pin
@@ -27,20 +30,24 @@ void deactivateSolenoid(int Pin) {
 
 void setup() {
   // put your setup code here, to run once:
-   Serial.begin(9600);
-    pinMode(digit_00, OUTPUT);
-    pinMode(digit_01, OUTPUT);
-    pinMode(digit_02, OUTPUT);
-    pinMode(digit_03, OUTPUT);
-    pinMode(digit_04, OUTPUT);
-    pinMode(digit_05, OUTPUT);
-    pinMode(digit_06, OUTPUT);
-    pinMode(digit_07, OUTPUT);
-    pinMode(digit_08, OUTPUT);
-    pinMode(digit_09, OUTPUT);
-    pinMode(Error_pin, OUTPUT);
-    pinMode(button_pin, INPUT);
-
+  //Serial.begin(9600);
+  pinMode(digit_00, OUTPUT);
+  pinMode(digit_01, OUTPUT);
+  pinMode(digit_02, OUTPUT);
+  pinMode(digit_03, OUTPUT);
+  pinMode(digit_04, OUTPUT);
+  pinMode(digit_05, OUTPUT);
+  pinMode(digit_06, OUTPUT);
+  pinMode(digit_07, OUTPUT);
+  pinMode(digit_08, OUTPUT);
+  pinMode(digit_09, OUTPUT);
+  pinMode(Error_pin, OUTPUT);
+  pinMode(start_pin, INPUT);
+  pinMode(left_trigger_pin, INPUT);
+  pinMode(right_trigger_pin, INPUT);
+  pinMode(drive_pin_a, OUTPUT);
+  pinMode(drive_pin_b, OUTPUT);
+  pinMode(release_arms_pin, OUTPUT);
 }
 
 const unsigned long SPACING = 75;  // ms between presses
@@ -51,15 +58,30 @@ bool extended = false; // if extended is true one solenoid is extended
 int piIndex = 0; 
 unsigned long beginDelay;
 int pinNumber = 0;
-bool was_pressed = false;
+bool startPressed = false;
+bool atWall = false;
+int drivePins[2] = {drive_pin_a, drive_pin_b};
 
 void loop() {
 
-  while (!was_pressed)
-  {
-    if(digitalRead(button_pin)) was_pressed = true;
+  while (!startPressed) {
+    if(digitalRead(trigger_pin)) {
+      startPressed = true;
+      delay(1000);
+    }
   }
- 
+
+  // drive forward until both switches hit.
+  while (!atWall) {
+    drive(drivePins, drive_forward);
+    if (digitalRead(trigger_pin)) {
+      atWall = true;
+      delay(200);
+      brake(drive_pin_a, drive_pin_b);
+      deactivateSolenoid(release_arms_pin);
+    }
+  }
+
   if(act) {
     pinNumber = pgm_read_byte_near(bigPI + piIndex);
     
